4466d0fecfcc1dbceff7f0ad26aed4a8139f92ee
misbahhassan08 <misbahhassan08@gmail.com>
2023-06-09 19:31:01 +0500

    removing errors

--- src/components/shared/SidebarData.js
+++ src/components/shared/SidebarData.js
@@ -37,13 +37,6 @@ const SidebarData = [
     icon: Media.Images.sixthicon,
     cName: "nav-text",
   },
-  {
-    //test101
-    title: "Test101",
-    path: "/test101",
-    icon: Media.Images.sixthicon,
-    cName: "nav-text",
-  },
 
   {
     title: "Build Report",
