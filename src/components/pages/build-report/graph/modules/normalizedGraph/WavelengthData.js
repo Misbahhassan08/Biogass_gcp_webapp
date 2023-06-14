294ad87652c5e4c8980a2b1308940f4a1971cd22
misbahhassan08 <misbahhassan08@gmail.com>
2023-06-14 21:34:40 +0500

    export functions added and remove errors

--- src/components/pages/build-report/graph/modules/normalizedGraph/WavelengthData.js
+++ src/components/pages/build-report/graph/modules/normalizedGraph/WavelengthData.js
@@ -3,6 +3,9 @@ import { ResponsiveLine } from "@nivo/line";
 import dayjs from "dayjs";
 import { CSVLink } from "react-csv";
 import { Button } from "react-bootstrap";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
+import XMLExport from "../../../XMLExport";
 
 function WaveLengthGraphData(props) {
   const [finalData, setFinalData] = useState([]);
@@ -12,6 +15,7 @@ function WaveLengthGraphData(props) {
   const [rowData, setrowData] = useState([]);
   const isDashboard = false;
   let wave;
+  
 
   switch (props.wave) {
     case "Vio":
@@ -19,6 +23,7 @@ function WaveLengthGraphData(props) {
       break;
     case "Blu":
       wave = "Blu_500nm";
+      
       break;
     case "Grn":
       wave = "Grn_550nm";
@@ -34,11 +39,28 @@ function WaveLengthGraphData(props) {
       break;
     default:
       break;
+    
   }
-
+  const fieldsAsObjects = {
+    "timestamp":"Date column header",
+    wave:"color data column header"
+  };
   useEffect(() => {
     showGraphData();
   }, []);
+
+
+  function handleExportImage() {
+    const graphContainer = document.getElementById("graph-container"+props.dataType +wave);
+    domtoimage
+      .toBlob(graphContainer)
+      .then((blob) => {
+        saveAs(blob, "graph.png");
+      })
+      .catch((error) => {
+        console.error("Error exporting image:", error);
+      });
+  }
 
   function showGraphData() {
     const data = localStorage.getItem("mqttResponseDataNormalized");
@@ -107,117 +129,128 @@ function WaveLengthGraphData(props) {
   const csvReport = {
     data: rowData,
     headers: headers,
+    xsd_filename:  props.dataType+'data_Normalized_Avg'+wave+'.xml',
     filename: props.dataType+'data_Normalized_Avg'+wave+'.csv'
   };
   return (
-    <div style={{ height: "60vh" }}>
+    <div >
       <h3 style={{ marginTop: 90, textAlign: "center" }}>
         {props.dataType} data : Normalized @ Avg_{wave}
       </h3>
-      <Button><CSVLink {...csvReport}>Export Data to CSV</CSVLink></Button>
-      <ResponsiveLine
-        data={finalData}
-        theme={{
-          axis: {
-            domain: {
-              line: {
-                stroke: "grey",
-              },
-            },
-            legend: {
-              text: {
-                fill: "grey",
-              },
-            },
-            ticks: {
-              line: {
-                stroke: "grey",
-                strokeWidth: 1,
-              },
-              text: {
-                fill: "grey",
-              },
-            },
-          },
-          legends: {
-            text: {
-              fill: "grey",
-            },
-          },
-          tooltip: {
-            container: {
-              color: "grey",
-            },
-          },
-        }}
-        colors={{ scheme: "nivo" }}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
-          min: "auto",
-          max: "auto",
-          stacked: false,
-          reverse: false,
-        }}
-        yFormat=" >-.2f"
-        curve="catmullRom"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          orient: "bottom",
-          tickSize: 7,
-          tickPadding: 5,
-          tickRotation: 40,
-          legend: isDashboard ? undefined : "Time", // added
-          legendOffset: 46,
-          legendPosition: "middle",
-        }}
-        axisLeft={{
-          orient: "left",
-          tickValues: 5, // added
-          tickSize: 7,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: isDashboard ? undefined : "Value", // added
-          legendOffset: -40,
-          legendPosition: "middle",
-        }}
-        enableGridX={false}
-        enableGridY={false}
-        pointSize={8}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabelYOffset={-12}
-        useMesh={true}
-        legends={[
-          {
-            anchor: "bottom-right",
-            direction: "column",
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: "left-to-right",
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: "circle",
-            symbolBorderColor: "rgba(0, 0, 0, .5)",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemBackground: "rgba(0, 0, 0, .03)",
-                  itemOpacity: 1,
+      <Button type="submit" className="mx-2 menu-btn menu-btn1"><CSVLink {...csvReport}>Export Data to CSV</CSVLink></Button>
+      <Button
+        type="submit"
+        className="mx-2 menu-btn menu-btn2"
+        onClick={handleExportImage}
+      >
+        Export Image
+      </Button>
+      <XMLExport data={rowData} fields={fieldsAsObjects} fileName={csvReport.xsd_filename} />
+      <div id = {"graph-container"+props.dataType + wave} style={{ height: "60vh" }}>
+        <ResponsiveLine
+          data={finalData}
+          theme={{
+            axis: {
+              domain: {
+                line: {
+                  stroke: "grey",
                 },
               },
-            ],
-          },
-        ]}
-      />
+              legend: {
+                text: {
+                  fill: "grey",
+                },
+              },
+              ticks: {
+                line: {
+                  stroke: "grey",
+                  strokeWidth: 1,
+                },
+                text: {
+                  fill: "grey",
+                },
+              },
+            },
+            legends: {
+              text: {
+                fill: "grey",
+              },
+            },
+            tooltip: {
+              container: {
+                color: "grey",
+              },
+            },
+          }}
+          colors={{ scheme: "nivo" }}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: "point" }}
+          yScale={{
+            type: "linear",
+            min: "auto",
+            max: "auto",
+            stacked: false,
+            reverse: false,
+          }}
+          yFormat=" >-.2f"
+          curve="catmullRom"
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            orient: "bottom",
+            tickSize: 7,
+            tickPadding: 5,
+            tickRotation: 40,
+            legend: isDashboard ? undefined : "Time", // added
+            legendOffset: 46,
+            legendPosition: "middle",
+          }}
+          axisLeft={{
+            orient: "left",
+            tickValues: 5, // added
+            tickSize: 7,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: isDashboard ? undefined : "Value", // added
+            legendOffset: -40,
+            legendPosition: "middle",
+          }}
+          enableGridX={false}
+          enableGridY={false}
+          pointSize={8}
+          pointColor={{ theme: "background" }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: "serieColor" }}
+          pointLabelYOffset={-12}
+          useMesh={true}
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: "left-to-right",
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: "circle",
+              symbolBorderColor: "rgba(0, 0, 0, .5)",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemBackground: "rgba(0, 0, 0, .03)",
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const rackStatusEndPoint = "https://biomass-gcp-server-rnt37kunua-uc.a.run.app/api/rack-status"
 export const baseApiUrl = "https://biomass-gcp-server-rnt37kunua-uc.a.run.app"
+export const rackStatusEndPoint = baseApiUrl + "/api/rack-status"
