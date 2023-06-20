5470fc93729478ffa54e85271300a9943049bb14
Misbah UL Hassan <74838817+Misbahhassan08@users.noreply.github.com>
2023-06-20 16:32:25 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const baseApiUrl = "http://127.0.0.1:8000" //"https://biomass-gcp-server-rnt37kunua-uc.a.run.app"
+export const baseApiUrl = "https://biomass-gcp-server-rnt37kunua-uc.a.run.app"
 export const rackStatusEndPoint = baseApiUrl + "/api/rack-status"
